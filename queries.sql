860572d7b3180d42e7110fe77d4aafb8140b68c8
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-22 15:38:55 -0600

    --List the date of birth of all animals named either Agumon or Pikachu.

--- queries.sql
+++ queries.sql
@@ -5,3 +5,5 @@ SELECT * FROM animals WHERE name LIKE '%mon%';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
 --List the name of all animals that are neutered and have less than 3 escape attempts.
 SELECT name FROM animals WHERE neutered AND scape_attempts < 3;
+--List the date of birth of all animals named either "Agumon" or "Pikachu".
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon','Pikachu');
