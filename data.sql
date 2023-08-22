95491120ebdb21a90cb3e92811f52797ebc4cdbd
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-22 10:12:21 -0600

    Add first value to table animals

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
+INSERT INTO animals (name, date_of_birth,  weight_kg, neutered, scape_attempts)
+VALUES ('Agumon', '2020-02-03', 10.23,true, 0);
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
