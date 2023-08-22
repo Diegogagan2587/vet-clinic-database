5cf3be12e0cc19113a1f71246dc214bb2f02e0f1
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-22 15:29:28 -0600

    Fix Error: Should list only the name

--- queries.sql
+++ queries.sql
@@ -1,5 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon%';
---List the name of all animals born between 2016 and 2019.
-SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+--List the name of all animals that are neutered and have less than 3 escape attempts.
+SELECT * FROM animals WHERE 
