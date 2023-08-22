d76facda9b9b324fd85170d0d00e016906d5e4cf
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-21 21:22:39 -0600

    Add notes for creating new database

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
+-- 1. First we create the new data base
+     -- createdb vet_clinic;
+     
+-- 2. Then we Create the new Table;
 
 CREATE TABLE animals (
-    name varchar(100)
+    id int,
+    name varchar(100),
+
 );
