ef3bd80756d32b9884a3b18adf3f27926bf74587
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-22 15:35:08 -0600

    --List the name of all animals that are neutered and have less than 3 escape attempts.

--- queries.sql
+++ queries.sql
@@ -4,4 +4,4 @@ SELECT * FROM animals WHERE name LIKE '%mon%';
 
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
 --List the name of all animals that are neutered and have less than 3 escape attempts.
-SELECT * FROM animals WHERE 
+SELECT name FROM animals WHERE neutered AND scape_attempts < 3;
