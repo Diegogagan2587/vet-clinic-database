b03f4c97535ab2f22faf020c9dc6539bc245ae2f
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-21 21:56:04 -0600

    template for creating new table

--- schema.sql
+++ schema.sql
@@ -1,11 +1,14 @@
 /* Database schema to keep the structure of entire database. */
 -- 1. First we create the new data base
      -- createdb vet_clinic;
-     
+
 -- 2. Then we Create the new Table;
 
 CREATE TABLE animals (
-    id int,
-    name varchar(100),
-
+    id INT,
+    name VARCHAR(100),
+    date_of_birth DATE,
+    scape_attempts INT, 
+    neutered BIT,
+    weight_kg DECIMAL(16,3);
 );
