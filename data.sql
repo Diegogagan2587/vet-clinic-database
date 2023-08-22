a5fed2949044a7257d27badd70933527256c195c
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-22 10:24:52 -0600

    Add animals data

--- data.sql
+++ data.sql
@@ -2,5 +2,12 @@
 
 INSERT INTO animals (name, date_of_birth,  weight_kg, neutered, scape_attempts)
 VALUES ('Agumon', '2020-02-03', 10.23,true, 0);
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+
+INSERT INTO animals (name, date_of_birth,  weight_kg, neutered, scape_attempts)
+VALUES ('Gabumon', '2018-11-15', 8,true, 2);
+
+INSERT INTO animals (name, date_of_birth,  weight_kg, neutered, scape_attempts)
+VALUES ('Pikachu', '2021-01-07', 15.04,false, 1);
+
+INSERT INTO animals (name, date_of_birth,  weight_kg, neutered, scape_attempts)
+VALUES ('Devimon', '2017-05-12', 11,true, 5);
