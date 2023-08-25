0383a5bcc0d1730827b4e75125d2c5e40b429119
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-24 18:21:16 -0600

    Add columns species_id and owner_id as foreign key references, create owners and species tables

--- schema.sql
+++ schema.sql
@@ -14,3 +14,32 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species VARCHAR(100);
+
+---->> Lesson: Query multiple tables start here <<----
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(250),
+    age INT,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250),
+    PRIMARY KEY (id)
+);
+
+
+ALTER TABLE animals ADD CONSTRAINT animalsPrimaryKey PRIMARY KEY (id);--make sure id in animals is primary key
+
+ALTER TABLE animals DROP species;
+
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+---->> Lesson: Query multiple tables ends here <<----
