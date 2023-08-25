14c222e177cdbe43b9f3725a7f181b03efef516b
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-24 20:22:27 -0600

    Insert data in tables: owners and species, update animals table

--- data.sql
+++ data.sql
@@ -32,3 +32,27 @@ VALUES ('Blossom','1998-10-13',17,true,3);
 
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered,scape_attempts)
 VALUES ('Ditto','2022-05-14',22,true,4);
+
+---->> Lesson: Query multiple tables starts here <<----
+INSERT INTO owners ( full_name, age)
+VALUES ('Sam Smith ',34),
+       ('Jennifer Orwell',19),
+       ('Bob',45),
+       ('Melody Pond',77),
+       ('Dean Winchester ',14),
+       ('Jodie Whittaker',38);
+
+INSERT INTO species (name) 
+VALUES ('Pokemon'),('Digimon');
+
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon%';
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+SELECT * FROM animals;
+
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander','Squirtle','Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon','Boarmon');
+
+---->> Lesson: Query multiple tables ends here <<----
