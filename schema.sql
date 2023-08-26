9164219557ce02106e33a6c16c0c89e7a87e449a
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-25 18:33:09 -0600

    Add link table called specializations

--- schema.sql
+++ schema.sql
@@ -51,4 +51,12 @@ CREATE TABLE vets (
     date_of_graduation DATE,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE specializations (
+    species_id INT,
+    vets_id INT,
+    CONSTRAINT pk_specializations PRIMARY KEY (species_id,vets_id),
+    CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id),
+    CONSTRAINT fk_vets FOREIGN KEY (vets_id) REFERENCES vets(id)
+);
 ---->> Lesson: Add "Join table" for visits Ends here <<----
