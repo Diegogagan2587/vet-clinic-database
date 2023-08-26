f7ecface6041257a97555f00a07aa0e75fd4c8b1
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-25 18:58:16 -0600

    add data for link table specializations

--- data.sql
+++ data.sql
@@ -62,4 +62,10 @@ VALUES ('William Tatcher', 45, '2000-04-23'),
        ('Maisy Smith', 26, '2019-01-17'),
        ('Stephanie Mendez', 64, '1981-05-04'),
        ('Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO specializations (vets_id, species_id)
+VALUES (1,1),
+       (3,2),
+       (3,1),
+       (4,2);
 ---->> Lesson: Add "Join table" for visits Ends here <<----
