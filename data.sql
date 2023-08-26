0070c6fb3515d1ae5e1ff04a1967b933b4488ca9
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-25 18:49:50 -0600

    Insert data into vets table

--- data.sql
+++ data.sql
@@ -57,4 +57,9 @@ UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon','Boarmon');
 
 ---->> Lesson: Query multiple tables ends here <<----
 ---->> Lesson: Add "Join table" for visits start here <<----
+INSERT INTO vets (name, age, date_of_graduation)
+VALUES ('William Tatcher', 45, '2000-04-23'),
+       ('Maisy Smith', 26, '2019-01-17'),
+       ('Stephanie Mendez', 64, '1981-05-04'),
+       ('Jack Harkness', 38, '2008-06-08');
 ---->> Lesson: Add "Join table" for visits Ends here <<----
